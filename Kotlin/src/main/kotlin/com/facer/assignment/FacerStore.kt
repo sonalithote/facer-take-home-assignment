25e41cf54b487810d823b9f9ecb33a5ba4ab6db2
Sonali <sonali@svkln.local>
2025-02-17 18:39:26 -0800

    Refactored Gunther's code with documentation

--- Kotlin/src/main/kotlin/com/facer/assignment/FacerStore.kt
+++ Kotlin/src/main/kotlin/com/facer/assignment/FacerStore.kt
@@ -1,72 +1,91 @@
 package com.facer.assignment
 
+/**
+* Manages the in store items and updates their quality and sellIn values
+* @param items the list of items in the store
+*/
 class FacerStore(var items: List<Item>) {
 
+    companion object {
+        private const val MAX_QUALITY = 50
+        private const val MIN_QUALITY = 0
+        private const val LEGENDARY_QUALITY = 80
+    }
+
+    /**
+    * Updates the quality and sellIn values of the items in the store
+    * - Vintage Rolex: increases in quality as it gets older
+    * - Passes to Watchface Conference: increases in quality as the event approaches
+    * - Fragile Watch: decreases in quality twice as fast as normal items
+    * - Default item: decreases in quality as it gets older
+    */
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Vintage Rolex" && items[i].name != "Passes to Watchface Conference") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Legendary Watch Face") {
-                        items[i].quality = items[i].quality - 1
-                        // Fragile watch check
-                        if (items[i].name == "Fragile Watch") {
-                            //if (items[i].sellIn < 0) {
-                            items[i].quality = items[i].quality - 1
-                            //}
-                        }
-                    }
+        for (item in items) {
+            when (item.name) {
+                "Legendary Watch Face" -> {
+                    // do nothing
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Passes to Watchface Conference") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+                "Vintage Rolex" -> updateVintageRolex(item)
+                "Passes to Watchface Conference" -> updatePassesToWatchfaceConference(item)
+                "Fragile Watch" -> updateFragileWatch(item)
+                else -> updateDefaultItem(item)
             }
-
-            if (items[i].name != "Legendary Watch Face") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Vintage Rolex") {
-                    if (items[i].name != "Passes to Watchface Conference") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Legendary Watch Face") {
-                                items[i].quality = items[i].quality - 1
-                                // Fragile watch check
-                                if (items[i].name == "Fragile Watch") {
-                                    items[i].quality = items[i].quality - 1
-                                }
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
-            // Fragile watch check
-            if (items[i].quality < 0) {
-                items[i].quality = 0
+            if (item.name != "Legendary Watch Face") {
+                item.sellIn--
             }
         }
     }
 
+    private fun updateVintageRolex(item: Item) {
+        if (item.quality < MAX_QUALITY) {
+            item.quality++
+            if (item.sellIn <= 0 && item.quality < MAX_QUALITY) {
+                item.quality++
+            }
+        }
+    }
+
+    private fun updatePassesToWatchfaceConference(item: Item) {
+        if (item.quality < MAX_QUALITY) {
+            item.quality++
+            if (item.sellIn <= 10 && item.quality < MAX_QUALITY) {
+                item.quality++
+            }
+            if (item.sellIn <= 5 && item.quality < MAX_QUALITY) {
+                item.quality++
+            }
+        }
+        if (item.sellIn <= 0) {
+            item.quality = MIN_QUALITY
+        }
+    }
+
+    /** 
+    * Fragile Watch: decreases in quality twice as fast as normal items
+    * - check if quality is greater than 0
+    * - decrease quality by 2
+    * - if sellIn is less than or equal to 0, decrease quality by 2
+    * - set quality to 0 if it is less than 0
+     */
+    private fun updateFragileWatch(item: Item) {
+        if (item.quality > 0) {
+            item.quality -= 2
+            if (item.sellIn <= 0) {
+                item.quality -= 2
+            }
+        }
+        item.quality = Math.max(MIN_QUALITY, item.quality)
+    }
+
+    private fun updateDefaultItem(item: Item) {
+        if (item.quality > 0) {
+            item.quality--
+            if (item.sellIn <= 0) {
+                item.quality--
+            }
+        }
+    }
 }
+
+
 
